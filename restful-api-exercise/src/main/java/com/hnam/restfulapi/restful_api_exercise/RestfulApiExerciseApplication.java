2b1ef5bfdd2c623df50bf751f4dbcc7355ad58a3
lamphnam <nampham2131@gmail.com>
2025-02-28 19:06:28 +0700

    first commit

---
+++ restful-api-exercise/src/main/java/com/hnam/restfulapi/restful_api_exercise/RestfulApiExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.hnam.restfulapi.restful_api_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiExerciseApplication.class, args);
+	}
+
+}
---
+++ restful-api-exercise/src/test/java/com/hnam/restfulapi/restful_api_exercise/RestfulApiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnam.restfulapi.restful_api_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
